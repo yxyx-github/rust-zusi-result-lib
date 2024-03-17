03bd50589df7ba4fa9d3acf562d404cc29cac1b1
yxyx-github <dev_mw@vodafonemail.de>
2024-03-17 20:33:56 +0100

    test for distance added

--- src/result_analyser.rs
+++ src/result_analyser.rs
@@ -34,3 +34,64 @@ impl ResultAnalyser {
         }
     }
 }
+
+#[cfg(test)]
+pub mod test {
+    use time::macros::datetime;
+    use zusi_xml_lib::xml::zusi::result::{ResultValue, ZusiResult};
+    use zusi_xml_lib::xml::zusi::result::fahrt_eintrag::{FahrtEintrag, FahrtTyp};
+    use crate::result_analyser::ResultAnalyser;
+
+    fn result() -> ZusiResult {
+        ZusiResult {
+            zugnummer: "12345".into(),
+            tf_nummer: "67890".into(),
+            datum: datetime!(2019-01-01 23:14),
+            verbrauch: 0.0,
+            bemerkung: "".to_string(),
+            schummel: false,
+            schwierigkeitsgrad: 0,
+            energie_vorgabe: 0.0,
+            value: vec![
+                ResultValue::FahrtEintrag(FahrtEintrag {
+                    fahrt_typ: FahrtTyp::Standard,
+                    fahrt_weg: 2.33,
+                    fahrt_zeit: datetime!(2019-01-01 23:18),
+                    fahrt_speed: 0.0,
+                    fahrt_speed_strecke: 0.0,
+                    fahrt_speed_signal: 0.0,
+                    fahrt_speed_zugsicherung: 0.0,
+                    fahrt_autopilot: false,
+                    fahrt_km: 0.0,
+                    fahrt_hl_druck: 0.0,
+                    fahrt_parameter: 0,
+                    fahrt_fpl_ank: None,
+                    fahrt_fpl_abf: None,
+                    fahrt_fb_schalter: 0,
+                }),
+                ResultValue::FahrtEintrag(FahrtEintrag {
+                    fahrt_typ: FahrtTyp::Standard,
+                    fahrt_weg: 22.43,
+                    fahrt_zeit: datetime!(2019-01-02 1:07),
+                    fahrt_speed: 0.0,
+                    fahrt_speed_strecke: 0.0,
+                    fahrt_speed_signal: 0.0,
+                    fahrt_speed_zugsicherung: 0.0,
+                    fahrt_autopilot: false,
+                    fahrt_km: 0.0,
+                    fahrt_hl_druck: 0.0,
+                    fahrt_parameter: 0,
+                    fahrt_fpl_ank: None,
+                    fahrt_fpl_abf: None,
+                    fahrt_fb_schalter: 0,
+                }),
+            ],
+        }
+    }
+
+    #[test]
+    fn test_distance() {
+        let analyser = ResultAnalyser::new(result());
+        assert_eq!(analyser.distance().unwrap(), 20.1);
+    }
+}
