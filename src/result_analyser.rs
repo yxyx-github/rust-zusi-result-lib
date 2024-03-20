143296dc1512baf8529f3aa8e0478cf482342f21
yxyx-github <dev_mw@vodafonemail.de>
2024-03-20 19:36:57 +0100

    fix test for average speed

--- src/result_analyser.rs
+++ src/result_analyser.rs
@@ -51,9 +51,10 @@ pub mod test {
     use time::macros::datetime;
     use zusi_xml_lib::xml::zusi::result::{ResultValue, ZusiResult};
     use zusi_xml_lib::xml::zusi::result::fahrt_eintrag::{FahrtEintrag, FahrtTyp};
+
     use crate::result_analyser::{AnalyseError, ResultAnalyser};
 
-    fn result(empty: bool) -> ZusiResult {
+    fn distance_result(empty: bool) -> ZusiResult {
         ZusiResult {
             zugnummer: "12345".into(),
             tf_nummer: "67890".into(),
@@ -71,23 +72,7 @@ pub mod test {
                         fahrt_typ: FahrtTyp::Standard,
                         fahrt_weg: 2.33,
                         fahrt_zeit: datetime!(2019-01-01 23:18),
-                        fahrt_speed: 74.9,
-                        fahrt_speed_strecke: 0.0,
-                        fahrt_speed_signal: 0.0,
-                        fahrt_speed_zugsicherung: 0.0,
-                        fahrt_autopilot: false,
-                        fahrt_km: 0.0,
-                        fahrt_hl_druck: 0.0,
-                        fahrt_parameter: 0,
-                        fahrt_fpl_ank: None,
-                        fahrt_fpl_abf: None,
-                        fahrt_fb_schalter: 0,
-                    }),
-                    ResultValue::FahrtEintrag(FahrtEintrag {
-                        fahrt_typ: FahrtTyp::Standard,
-                        fahrt_weg: 5.3,
-                        fahrt_zeit: datetime!(2019-01-01 23:18),
-                        fahrt_speed: 64.9,
+                        fahrt_speed: 0.0,
                         fahrt_speed_strecke: 0.0,
                         fahrt_speed_signal: 0.0,
                         fahrt_speed_zugsicherung: 0.0,
@@ -103,7 +88,7 @@ pub mod test {
                         fahrt_typ: FahrtTyp::Standard,
                         fahrt_weg: 22.43,
                         fahrt_zeit: datetime!(2019-01-02 1:07),
-                        fahrt_speed: 3.0,
+                        fahrt_speed: 0.0,
                         fahrt_speed_strecke: 0.0,
                         fahrt_speed_signal: 0.0,
                         fahrt_speed_zugsicherung: 0.0,
@@ -122,19 +107,80 @@ pub mod test {
 
     #[test]
     fn test_distance() {
-        let analyser = ResultAnalyser::new(result(false));
+        let analyser = ResultAnalyser::new(distance_result(false));
         assert_eq!(analyser.distance().unwrap(), 20.1);
     }
 
     #[test]
     fn test_distance_with_empty_result() {
-        let analyser = ResultAnalyser::new(result(true));
+        let analyser = ResultAnalyser::new(distance_result(true));
         assert_eq!(analyser.distance(), Err(AnalyseError::NoEntriesFound));
     }
 
     #[test]
     fn test_average_speed() {
-        let analyser = ResultAnalyser::new(result(false));
-        assert_eq!(analyser.average_speed().unwrap(), 39.848977900);
+        let result = ZusiResult {
+            zugnummer: "12345".into(),
+            tf_nummer: "67890".into(),
+            datum: datetime!(2019-01-01 23:14),
+            verbrauch: 0.0,
+            bemerkung: "".to_string(),
+            schummel: false,
+            schwierigkeitsgrad: 0,
+            energie_vorgabe: 0.0,
+            value: vec![
+                    ResultValue::FahrtEintrag(FahrtEintrag {
+                        fahrt_typ: FahrtTyp::Standard,
+                        fahrt_weg: 5.0,
+                        fahrt_zeit: datetime!(2019-01-01 23:18),
+                        fahrt_speed: 10.0,
+                        fahrt_speed_strecke: 0.0,
+                        fahrt_speed_signal: 0.0,
+                        fahrt_speed_zugsicherung: 0.0,
+                        fahrt_autopilot: false,
+                        fahrt_km: 0.0,
+                        fahrt_hl_druck: 0.0,
+                        fahrt_parameter: 0,
+                        fahrt_fpl_ank: None,
+                        fahrt_fpl_abf: None,
+                        fahrt_fb_schalter: 0,
+                    }),
+                    ResultValue::FahrtEintrag(FahrtEintrag {
+                        fahrt_typ: FahrtTyp::Standard,
+                        fahrt_weg: 15.0,
+                        fahrt_zeit: datetime!(2019-01-01 23:18),
+                        fahrt_speed: 30.0,
+                        fahrt_speed_strecke: 0.0,
+                        fahrt_speed_signal: 0.0,
+                        fahrt_speed_zugsicherung: 0.0,
+                        fahrt_autopilot: false,
+                        fahrt_km: 0.0,
+                        fahrt_hl_druck: 0.0,
+                        fahrt_parameter: 0,
+                        fahrt_fpl_ank: None,
+                        fahrt_fpl_abf: None,
+                        fahrt_fb_schalter: 0,
+                    }),
+                    ResultValue::FahrtEintrag(FahrtEintrag {
+                        fahrt_typ: FahrtTyp::Standard,
+                        fahrt_weg: 35.0,
+                        fahrt_zeit: datetime!(2019-01-02 1:07),
+                        fahrt_speed: 100.0,
+                        fahrt_speed_strecke: 0.0,
+                        fahrt_speed_signal: 0.0,
+                        fahrt_speed_zugsicherung: 0.0,
+                        fahrt_autopilot: false,
+                        fahrt_km: 0.0,
+                        fahrt_hl_druck: 0.0,
+                        fahrt_parameter: 0,
+                        fahrt_fpl_ank: None,
+                        fahrt_fpl_abf: None,
+                        fahrt_fb_schalter: 0,
+                    }),
+                ],
+        };
+
+        let analyser = ResultAnalyser::new(result);
+        assert_eq!(analyser.average_speed().unwrap(), 50.0);
     }
 }
